8f55f007d147bbb717915977cbc79627be8107ef
ttuluk <ttasya.k@gmail.com>
2021-09-14 15:21:54 +0300

    upgrate timer

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 
-const refs = {
-  startBtn: document.querySelector('button[data-action-start]'),
-  stopBtn: document.querySelector('button[data-action-stop]'),
-  clockface: document.querySelector('.js-clockface'),
-};
+import refs from './refs.js';
+const { daysEl, hoursEl, minsEl, secondsEl, startBtn, stopBtn } = refs;
+
 const DATE = 'Jul 17, 2022';
-class Timer {
+class CountdownTimer {
   constructor({ onTick }) {
     this.intervalId = null;
     this.isActive = false;
@@ -43,12 +41,7 @@ class Timer {
     this.onTick(time);
   }
 
-  /*
-   * - Принимает время в миллисекундах
-   * - Высчитывает сколько в них вмещается часов/минут/секунд
-   * - Возвращает обьект со свойствами hours, mins, secs
-   * - Адская копипаста со стека 💩
-   */
+
     getTimeComponents(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
     const hours = this.pad(
@@ -60,28 +53,28 @@ class Timer {
     return {days, hours, mins, secs };
   }
 
-  /*
-   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
-   */
   pad(value) {
     return String(value).padStart(2, '0');
   }
 };
 
-const timer = new Timer({
+const timer = new CountdownTimer({
     onTick: updateClockface,
     selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date(DATE),
 });
 
 refs.startBtn.addEventListener('click', timer.start.bind(timer));
 refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
 
-/*
- * - Принимает время в миллисекундах
- * - Высчитывает сколько в них вмещается часов/минут/секунд
- * - Рисует интерфейс
- */
+function insertData(place, value) {
+  place.textContent = value;
+}
+
 function updateClockface({ days, hours, mins, secs }) {
-  refs.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
+
+  insertData(daysEl, days);
+  insertData(hoursEl, hours);
+  insertData(minsEl, mins);
+  insertData(secondsEl, secs);
 };
---
+++ js/refs.js
@@ -0,0 +1,11 @@
+export default {
+  daysEl: document.querySelector('[data-value="days"]'),
+  hoursEl: document.querySelector('[data-value="hours"]'),
+  minsEl: document.querySelector('[data-value="mins"]'),
+  secondsEl: document.querySelector('[data-value="secs"]'),
+
+  startBtn: document.querySelector('button[data-action-start]'),
+stopBtn: document.querySelector('button[data-action-stop]'),
+   clockface: document.querySelector('.js-clockface'),
+}
+
